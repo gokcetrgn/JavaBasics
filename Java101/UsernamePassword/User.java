d1ebbc7dbe5ed715badd13c1e77b43693da16dae
Gokcenaz Torgan <gokcetorgan@hotmail.com>
2023-08-25 19:30:23 +0300

    username-password created

---
+++ Java101/UsernamePassword/User.java
@@ -0,0 +1,31 @@
+package UsernamePassword;
+
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+        String userName,password;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your username:");
+        userName = scanner.nextLine();
+
+        System.out.println("Enter your password");
+        password = scanner.nextLine();
+
+        System.out.println("Do you want to change your password? y/n");
+        String yesNo = scanner.nextLine();
+        if(yesNo.equals("y")){
+            System.out.println("Enter your new password: ");
+            String newPassword = scanner.nextLine();
+            if(newPassword.equals(password)){
+                System.out.println("Invalid value. Try again");
+            }else{
+                System.out.println("Success");
+            }
+        }
+        scanner.close();
+
+    }
+}
