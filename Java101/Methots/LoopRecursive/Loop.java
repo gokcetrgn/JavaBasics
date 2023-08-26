3f40646ccc8574388c46635dd8683c912e5a286a
Gokcenaz Torgan <gokcetorgan@hotmail.com>
2023-08-26 21:25:09 +0300

    loop examp created

---
+++ Java101/Methots/LoopRecursive/Loop.java
@@ -0,0 +1,33 @@
+package Methots.LoopRecursive;
+
+import java.util.Scanner;
+
+public class Loop {
+    static int sayi(int a, int b) {
+
+        if (a > 0) {
+            System.out.print(a + " ");
+            a -= 5;
+            return sayi(a,b);
+        }
+        return sayi1(a, b);
+
+    }
+    static int sayi1(int a, int b) {
+        if (a <= b) {
+            System.out.print(a + " ");
+            a += 5;
+            return sayi1(a, b);
+        }
+
+        return b;
+    }
+    public static void main(String[] args) {
+        int b,a;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        b = input.nextInt();
+        a = b ;
+        sayi(a,b);
+    }
+}
